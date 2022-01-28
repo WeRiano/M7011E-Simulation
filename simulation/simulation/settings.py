9693759e32e551c89e193e753dc8dd32824bbd26
root <williamfors13@gmail.com>
2022-01-28 00:13:57 +0000

    cleanup

--- simulation/simulation/settings.py
+++ simulation/simulation/settings.py
@@ -28,13 +28,7 @@ SECRET_KEY = str(os.environ.get('DJANGO_SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = []
-ALLOWED_HOSTS.extend(
-    filter(
-        None,
-        os.environ.get('ALLOWED_HOSTS', '').split(', ')
-    )
-)
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -49,7 +43,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
 
-     'api',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -64,11 +58,6 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOWED_ORIGINS = [
-    "http://" + os.environ.get("FRONTEND_IP", "127.0.0.1") + ":8001",
-    "http://" + os.environ.get("BACKEND_IP", "127.0.0.1") + ":7999"
-]
 
 CORS_ALLOWED_METHODS = [
     'GET',
